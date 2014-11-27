abf130810c0e7940af2d15ac14617796fbe9ef97
Simon Morris <simon@mrsimonmorris.co.uk>
2014-11-27 18:16:15 +0000

    text wrapping

--- uk/co/mojaworks/norman/components/display/text/TextSprite.hx
+++ uk/co/mojaworks/norman/components/display/text/TextSprite.hx
@@ -35,8 +35,8 @@ class TextSprite extends Sprite
 	public var wrapWidth( default, default ) : Float;
 	
 	// results
-	//public var lineLengths( default, null ) : Array<Float>;
-	//public var bounds( default, null ) : Rectangle;
+	private var _lineStops : Array<Int>;
+	private var _bounds : Rectangle;
 		
 	// Draws onto this texture constantly re-uses it
 	var _textureData : TextureData;
@@ -50,6 +50,7 @@ class TextSprite extends Sprite
 		color = 0xFFFFFFFF;
 		wrapWidth = 0;
 		align = TextAlign.Left;
+		_bounds = new Rectangle();
 	}
 	
 	/**
@@ -95,33 +96,132 @@ class TextSprite extends Sprite
 	 * Render
 	 * @param	canvas
 	 */
-			
-	override public function render(canvas:ICanvas):Void 
-	{
-
-		// TODO: render to texture for easy caching
-				
-		var lineStart : Int = 0;
-		var lineLength : Float = 0;	
-		var y = 0;
+	
+	private function regenerateLayout() : Void {
 		
+		_bounds.setEmpty();
+		_lineStops = [0];
+		
+		var lineStart : Int = 0;
+		var wordStart : Int = 0;
+		var lineLength : Float = 0;	
+		var lineLengthToLastWord : Float = 0;
+		var isFirstWord : Bool = true;
+				
+		trace("Starting layout", text.length, text);
 		
 		for ( i in 0...text.length ) {
 			
 			var char : CharacterData =  font.characters.get( text.charCodeAt(i) );
 			if ( char == null ) char = new CharacterData( text.charCodeAt(i) );
 			
-			//Draw characters a line at a time
-			if ( char.id != 10 && char.id != 13 && ( wrapWidth == 0 || lineLength + char.xAdvance < wrapWidth ) && i < text.length - 1 ) {
-				// We will draw this line all together
-				lineLength += font.characters.get( text.charCodeAt(i) ).xAdvance;
-				continue;
-			}else {
-				// draw the line
-				drawLine( canvas, text.substring( lineStart, i+1 ), y );
-				lineStart = i;
-				y += font.lineHeight;
+			if ( char.id == 32 || char.id == 10 ) {
+				wordStart = i + 1;
+				isFirstWord = false;
+				lineLengthToLastWord = lineLength;
+				trace( "Found a space, setting word start to", i );
 			}
+			
+			//Draw characters a line at a time
+			if ( i == text.length - 1 ) {
+				
+				if ( char.id != 10 && ( wrapWidth == 0 || lineLength + char.xAdvance < wrapWidth ) ) {
+					//drawLine( canvas, text.substring( lineStart ), y );
+					_bounds.width = Math.max( _bounds.width, lineLength );
+					trace( "Drawing last letter on existing line" );
+				}else {
+					_lineStops.push( wordStart );
+					_bounds.width = Math.max( _bounds.width, lineLengthToLastWord );
+					_bounds.width = Math.max( _bounds.width, lineLength - lineLengthToLastWord );
+					trace( "Last letter pushed last word onto new line" );
+				}
+				
+			}else if ( char.id != 10 && ( wrapWidth == 0 || (lineLength + char.xAdvance < wrapWidth) ) ) {
+				// We will draw this line all together
+				
+				
+				lineLength += char.xAdvance;
+				trace("Line length is now " + lineLength );
+				
+			}else {
+								
+				trace( "Creating new line", wordStart, text.substring( wordStart ) );
+						
+				if ( !isFirstWord ) {
+					_bounds.width = Math.max( _bounds.width, lineLengthToLastWord );
+					lineStart = wordStart;
+					lineLength = lineLength - lineLengthToLastWord;
+					_lineStops.push( wordStart );
+				}else {
+					lineStart = i;
+					_bounds.width = Math.max( _bounds.width, lineLength );
+					lineLength = 0;
+					_lineStops.push( i );
+				}
+				
+				if ( char.id != 10 ) {
+					wordStart = i;
+				}else {
+					wordStart = i + 1;
+				}
+				isFirstWord = true;
+			}
+		}
+		
+		_bounds.height = _lineStops.length * font.lineHeight;
+		_dirty = false;
+		
+		trace("Final calculation", _lineStops, _bounds );
+		
+	}
+			
+	override public function render(canvas:ICanvas):Void 
+	{
+
+		// TODO: render to texture for easy caching
+		if ( _dirty ) regenerateLayout();
+				
+		var lineStart : Int = 0;
+		var wordStart : Int = 0;
+		var lineLength : Float = 0;	
+		var y = 0;
+		
+		//trace("Drawing", _lineStops, _bounds );
+		
+		for ( i in 0..._lineStops.length ) {
+			
+			if ( i == _lineStops.length - 1 ) {
+				drawLine( canvas, text.substring( _lineStops[i] ), i * font.lineHeight );
+			}else {
+				drawLine( canvas, text.substring( _lineStops[i], _lineStops[i+1] ), i * font.lineHeight );
+			}
+			
+			//var char : CharacterData =  font.characters.get( text.charCodeAt(i) );
+			//if ( char == null ) char = new CharacterData( text.charCodeAt(i) );
+			//
+			//if ( char.id == 32 ) wordStart = i + 1;
+			////Draw characters a line at a time
+			//if ( i == text.length - 1 ) {
+				//
+				//if ( ( wrapWidth == 0 || lineLength + char.xAdvance < wrapWidth ) ) {
+					//drawLine( canvas, text.substring( lineStart ), y );
+				//}else {
+					//drawLine( canvas, text.substring( lineStart, wordStart - 1 ), y );
+					//drawLine( canvas, text.substring( wordStart ), y + font.lineHeight );
+				//}
+			//}
+			//if ( char.id != 10 && char.id != 13 && ( wrapWidth == 0 || lineLength + char.xAdvance < wrapWidth ) ) {
+				//// We will draw this line all together
+				//lineLength += font.characters.get( text.charCodeAt(i) ).xAdvance;
+				//continue;
+				//
+			//}else {
+				//// draw the line
+				//drawLine( canvas, text.substring( lineStart, wordStart - 1 ), y );
+				//lineStart = wordStart;
+				//lineLength = 0;
+				//y += font.lineHeight;
+			//}
 		}
 	}
 	
@@ -131,6 +231,33 @@ class TextSprite extends Sprite
 		
 		var x : Float = 0;
 		var prev_char : CharacterData = null;
+		
+		// First go through and get line length
+		for ( i in 0...string.length ) {
+			
+			var char : CharacterData =  font.characters.get( string.charCodeAt(i) );
+			if ( char != null ) {
+				
+				var kerning : KerningData = null;
+				if ( prev_char != null && font.kernings.get( prev_char.id ) != null ) {
+					kerning = font.kernings.get( prev_char.id ).get( char.id );
+					if ( kerning != null ) x -= kerning.amount;
+				}
+				x += char.xAdvance;
+			}
+			prev_char = char;				 
+		}
+		
+		switch( align ) {
+			case TextAlign.Left:
+				x = 0;
+			case TextAlign.Center:
+				x = (wrapWidth - x) * 0.5;
+			case TextAlign.Right:
+				x = wrapWidth - x;
+		}
+		
+		prev_char = null;
 		
 		// TODO: align this line to the left, right or center
 		for ( i in 0...string.length ) {
@@ -168,25 +295,6 @@ class TextSprite extends Sprite
 		
 	}
 	
-	/**
-	 * Gets the width of the text after layout
-	 * @return
-	 */
-	
-	override public function getNaturalWidth() : Float 
-	{
-		return 0;
-	}
-	
-	/**
-	 * Gets the height of the text after layout
-	 * @return
-	 */
-	
-	override public function getNaturalHeight() : Float 
-	{
-		return 0;
-	}
 	
 	/**
 	 * Sets the font color
@@ -212,20 +320,40 @@ class TextSprite extends Sprite
 	
 	public function set_text( text : String ) : String {
 		// Make all new lines the same
+		var old_text : String = this.text;
 		this.text = StringTools.replace( text, "\r\n", "\n" );
 		this.text = StringTools.replace( this.text, "\r", "\n" );
+		if ( old_text != text ) _dirty = true;
 		return this.text;
 	}
-	//public function setAlign( align : TextAlign ) : TextSprite {
-		//this.align = align;
-		//_dirty = true;
-		//return this;
-	//}
-	//
-	//public function setText( text : String ) : TextSprite {
-		//this.text = text;
-		//_dirty = true;
-		//return this;
-	//}	
+	
+	public function setAlign( align : TextAlign ) : TextSprite {
+		this.align = align;
+		_dirty = true;
+		return this;
+	}
+	
+	public function setText( text : String ) : TextSprite {
+		this.text = text;
+		_dirty = true;
+		return this;
+	}	
+	
+	/**
+	 * Sizes
+	 * @return
+	 */
+	
+	override public function getNaturalWidth() : Float 
+	{
+		if ( _dirty ) regenerateLayout();
+		return _bounds.width;
+	}
+	
+	override public function getNaturalHeight() : Float 
+	{
+		if ( _dirty ) regenerateLayout();
+		return _bounds.height;
+	}
 	
 }
