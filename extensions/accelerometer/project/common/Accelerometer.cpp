808e4fa1a32f206323eca284908df6d66cf31fe8
unknown <smorris@PC15.R1G.local>
2015-05-28 13:53:11 +0100

    start extensions

---
+++ extensions/accelerometer/project/common/Accelerometer.cpp
@@ -0,0 +1,14 @@
+#include "Utils.h"
+
+
+namespace accelerometer {
+	
+	
+	int SampleMethod(int inputValue) {
+		
+		return inputValue * 100;
+		
+	}
+	
+	
+}
---
+++ extensions/accelerometer/project/include/Utils.h
@@ -0,0 +1,14 @@
+#ifndef SAMPLEEXTENSION_H
+#define SAMPLEEXTENSION_H
+
+
+namespace accelerometer {
+	
+	
+	int SampleMethod(int inputValue);
+	
+	
+}
+
+
+#endif
