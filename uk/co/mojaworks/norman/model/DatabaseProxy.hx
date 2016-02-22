5f6af426fb1cac094cb5e804be1b3f817da4ba32
unknown <smorris@PC15.R1G.local>
2016-02-22 13:27:59 +0000

    Database proxy

---
+++ uk/co/mojaworks/norman/model/DatabaseProxy.hx
@@ -0,0 +1,48 @@
+package uk.co.mojaworks.norman.model;
+
+import haxe.ds.StringMap;
+import lime.Assets;
+import lime.system.System;
+import lime.utils.Bytes;
+import sys.io.File;
+import sys.io.FileOutput;
+import uk.co.mojaworks.norman.db.SQLiteDB;
+import uk.co.mojaworks.norman.systems.model.Proxy;
+
+/**
+ * ...
+ * @author Simon
+ */
+class DatabaseProxy extends Proxy
+{
+	static public inline var ID:String = "DatabaseProxy";
+	
+	public var db:SQLiteDB;
+
+	public function new( asset : String ) 
+	{
+		super( ID );
+		
+		var filename : String = copyDBToLocal( asset, "main", true );
+		db = new SQLiteDB( filename );
+		
+	}
+	
+	private function copyDBToLocal( asset : String, dbName : String, overwriteIfExists : Bool ) : String 
+	{
+		var filename : String = System.applicationStorageDirectory + dbName + ".db";
+		var dbGame : Bytes = Assets.getBytes( asset );
+		var file : FileOutput = File.write( filename, true );
+		file.writeBytes( dbGame, 0, dbGame.length );
+		file.close();
+		
+		return filename;
+	}
+	
+	public function addDBFile( asset : String, overwriteIfExists : Bool, dbName : String ) : Void 
+	{
+		var filename : String = copyDBToLocal( asset, dbName, overwriteIfExists );
+		db.attach( filename, dbName );
+	}
+	
+}
